fd83c7dffff5c3e9fb910b164143bc19365c3a7b
Guillaume THOMAS <guillaumethomas@outlook.com>
2018-05-04 08:02:44 -0700

    Friday

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+'''
+binary search, also known as half-interval search,[1] logarithmic search,
+or binary chop, is a search algorithm that finds the position of a target
+value within a sorted array.[4][5] Binary search compares the target value
+to the middle element of the array; if they are unequal, the half in which
+the target cannot lie is eliminated and the search continues on the remaining
+half until it is successful. If the search ends with the remaining
+half being empty, the target is not in the array.
+'''
+
+
+if __name__ == "__main__":
---
+++ dijkstra.py
@@ -0,0 +1,6 @@
+'''
+Dijkstra's algorithm is an algorithm for finding the shortest
+paths between nodes in a graph
+'''
+
+if __name__ == "__main__":
